533a40987357472a18446b39d5cf6d312c4dc273
KPInc425 <kpinc425@gmail.com>
2023-09-18 14:37:50 -0700

    update logocard background color to match theme variable, update projects and projects component to be dynamic

--- src/components/PortfolioProject.tsx
+++ src/components/PortfolioProject.tsx
@@ -1,37 +1,43 @@
 /** @jsxImportSource theme-ui */
 import { Box, Flex, Image, Text } from "theme-ui"
 
-const PortfolioProject = () => {
+type PortfolioProjectProps = {
+  project: {
+    title: string,
+    tagline: string,
+    description: string,
+    bullets: string[],
+    imageUrl: string
+  }
+}
+
+const PortfolioProject = ({project}: PortfolioProjectProps) => {
   return (
     <>
       <Box mx='auto' sx={{borderRadius: '12px', backgroundColor: 'surfaceOpaque', height: '600px', width: '1100px', position: 'relative'}}>
         <Box sx={{borderRadius: '12px', backgroundColor: 'surfaceSecondary', height: '300px', width: '300px', position: 'absolute', left: '-20%', top: '12%'}}>
           <Flex sx={{justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100%'}}>
-          <Text sx={{ variant: 'text.heading', color: 'background'}}>PROJECT TITLE</Text>
-          <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', textAlign: 'center', padding: '12px 20px'}}>PROJECT TAGLINE SOME MORE CLEVER WORDS, YIPPIKIYAY!</Text>
+          <Text sx={{ variant: 'text.heading', color: 'background'}}>{project.title}</Text>
+          <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', textAlign: 'center', padding: '12px 20px'}}>{project.tagline}</Text>
           </Flex>
         </Box>
         
         <Flex sx={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
           <Flex px='6' sx={{flexDirection: 'column'}}>
             <Text sx={{ variant: 'text.heading', color: 'text'}}>Description</Text>
-            <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus. Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus.</Text>
+            <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>{project.description}</Text>
           </Flex>
           <Flex pt='4' sx={{alignItems: 'center'}}>
             {/* <ul> */}
               <Flex pl='6' sx={{flexDirection: 'column', width: '60%', gap: '4'}}>
+                {project.bullets.map((bullet) => (
                   <li>
-                    <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem.</Text>
+                    <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>{bullet}</Text>
                   </li>
-                  <li>
-                    <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.</Text>
-                  </li>
-                  <li>
-                    <Text sx={{ variant: 'text.infoCardDescription', color: 'grey', padding: '12px 20px'}}>Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.</Text>
-                  </li>
+                ))}
               </Flex>
             {/* </ul> */}
-            <Image src='https://via.placeholder.com/300' sx={{borderRadius: '12px', width: '300px', height: '200px'}}/>
+            <Image src={project.imageUrl} sx={{borderRadius: '12px', width: '300px', height: '200px'}}/>
           </Flex>
         </Flex>
 
--- src/components/PortfolioProjects.tsx
+++ src/components/PortfolioProjects.tsx
@@ -3,26 +3,51 @@ import { Box, Flex, Text } from "theme-ui"
 import PortfolioProject from "./PortfolioProject"
 import SlantedBackground from "./SlandtedBackground"
 
+const projects = [
+  {
+    title: 'Project Title',
+    tagline: 'PROJECT TAGLINE SOME MORE CLEVER WORDS, YIPPIKIYAY!',
+    description: 'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus. Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.'
+    ],
+    imageUrl: 'https://via.placeholder.com/300'
+  },
+  {
+    title: 'Project 2 Title',
+    tagline: 'PROJECT TAGLINE SOME MORE CLEVER WORDS, THIS IS A DIFFERENT PROJECT!',
+    description: 'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus. Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.'
+    ],
+    imageUrl: 'https://via.placeholder.com/300'
+  },
+  {
+    title: 'Project 3 Title',
+    tagline: 'PROJECT TAGLINE SOME MORE CLEVER WORDS, THIS IS A DIFFERENT PROJECT!',
+    description: 'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus. Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem qui galisum incidunt aut exercitationem voluptatem. Non galisum possimus quo dignissimos omnis At natus minima non omnis quidem qui soluta voluptatibus.',
+    bullets: [
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum ut facilis voluptatem.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.',
+      'Lorem ipsum dolor sit amet. Est tempora eligendi et dolores exercitationem aut nemo dolorum.'
+    ],
+    imageUrl: 'https://via.placeholder.com/300'
+  }
+]
+
 const PortfolioProjects = () => {
     return (
       <Flex mb='5' sx={{justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: '6', position: 'relative'}}>
-        {/* <SlantedBackground /> */}
-        <Flex sx={{position: 'relative', width: '99vw', height: '100vh', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', scrollSnapAlign: 'start'}}>
-          <SlantedBackground/>
-          <PortfolioProject />
-        </Flex>
-        <Flex sx={{position: 'relative', width: '99vw', height: '100vh', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', scrollSnapAlign: 'start'}}>
-          <SlantedBackground/>
-          <PortfolioProject />
-        </Flex>
-        <Flex sx={{position: 'relative', width: '99vw', height: '100vh', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', scrollSnapAlign: 'start'}}>
-          <SlantedBackground/>
-          <PortfolioProject />
-        </Flex>
-        <Flex sx={{position: 'relative', width: '99vw', height: '100vh', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', scrollSnapAlign: 'start'}}>
-          <SlantedBackground/>
-          <PortfolioProject />
-        </Flex>
+        {projects.map((project, index) => (
+          <Flex key={index} sx={{position: 'relative', width: '99vw', height: '100vh', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', scrollSnapAlign: 'start'}}>
+            <SlantedBackground/>
+            <PortfolioProject project={project}/>
+          </Flex>
+        ))}
       </Flex>
     )
 }
