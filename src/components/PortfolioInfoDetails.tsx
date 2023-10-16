0da8c91c37d93e793f94235f02ae887be2641504
KPInc425 <kpinc425@gmail.com>
2023-10-15 20:34:26 -0700

    Update some responsive styling for mobile

--- src/components/PortfolioInfoDetails.tsx
+++ src/components/PortfolioInfoDetails.tsx
@@ -10,13 +10,13 @@ const PortfolioInfoDetails = ({heading, description, graphicText} : PortfolioInf
     return (
         <Box sx={{borderRadius: '12px', backgroundColor: 'surfaceGlass', height: ['fit-content'], width: ['100%', '100%','fit-content'], maxWidth: '1000px'}}>
             <Flex sx={{justifyContent: ['center','space-between'], alignItems: ['center','space-between'], flexDirection: ['column', 'row']}}>
-                <Box pt={['4','100px']} px={['3','5']} sx={{width: ['100%','60%']}}>
+                <Box pt={['4','100px']} px={['4','5']} sx={{width: ['100%','60%'], textWrap: 'balance'}}>
                     <Text sx={{variant: 'text.infoCardHeading'}}>{heading}</Text>
                 </Box>
                 <Box mr='4' mt='4' sx={{border: '8px solid #20421A', borderRadius: '12px', height: '175px', width: ['100%','300px']}}>
                     <Box ml={['0','-5']} mt='4' sx={{backgroundColor: 'black', borderRadius: '12px', height: '175px', width: ['100%','300px']}}>
                         <Box ml='25px' sx={{display: 'relative', textAlign: ['center', 'start']}}>
-                            <Text mt={['0','-4']} sx={{variant: 'text.infoCardGraphic', display: 'inline-block', textShadow: '0 0 20px #15B400'}}>{graphicText}</Text>
+                            <Text mt={['0','-4']} p={['4', '0']} sx={{variant: 'text.infoCardGraphic', display: 'inline-block', textShadow: '0 0 20px #15B400'}}>{graphicText}</Text>
                         </Box>
                     </Box>
                 </Box>
